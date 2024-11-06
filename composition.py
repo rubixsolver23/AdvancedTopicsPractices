ffe42a538525aec0b3ff0e5fc50213fde391168c
rubixsolver23 <tylerclaywatts@gmail.com>
2024-11-06 11:19:26 -0700

    finished the notes

--- composition.py
+++ composition.py
@@ -10,6 +10,10 @@ class Car:
 
     def __str__(self):
         return f"This is a {self.year} {self.make} {self.model}"
+    
+    # for other programmers for debugging purposes. Tell the class and all the attributes.
+    def __repr__(self):
+        return f"Car({self.make}, {self.model}, {self.year}, {self.engine})"
 
 
 class Engine:
@@ -22,11 +26,17 @@ class Engine:
     def ignite(self):
         print("Vroom, vroom!")
 
+    def __str__(self):
+        return f"This engine is a {self.configuration} with {self.displacement} {self.horsepower} horsepower and {self.torque} torque"
+    
+    def __repr__(self):
+        return f"Engine({self.configuration}, {self.displacement}, {self.horsepower}, {self.torque})"
+
 # To call a composite class you must access where it is saved within the class
 myEngine = Engine("V8", 5.8, 326, 344)
 myCar = Car("Honda", "Clarity", 2016, myEngine)
 
 print(myCar)
 myCar.engine.ignite()
-
+print(repr(myCar))
 
